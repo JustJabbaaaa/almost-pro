e6c7f7ecb7ed691fcf3a8ce6ff67602585764eea
JustJabbaaaa <adriankotowski217@gmail.com>
2024-12-02 19:55:53 +0100

    team cards v2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,10 @@
   const pageTitle = 'Home';
   useHead({
     title: pageTitle,
-  });
+  }); 
+  function toggleCard(item) {
+    item.flipped = !item.flipped
+  }
 </script>
 
 <template>
@@ -18,14 +21,99 @@
         <NuxtImg src="/images/about/about_image.webp" alt="Section Image" height="800" width="800" loading="lazy" />
       </div>
     </Section>
-    <Section anchor="Teams" title="Drużyny" subtitle="Poznaj nasze drużyny" grid="4">
-      <div v-for="item in pierwsza" :key="item.title" class="teams__card">
-        <div class="teams__card--icon">
-          <NuxtImg :alt="`Logo drużyny ${item.team}`" :src="`/images/teams/${item.shortcut}.png`"/>
+    <Section anchor="Services" title="Drużyny" class="chooseLeague">
+            <div class="chooseLeague__buttons">
+            <button :class="{'active': selectedDistrict === 'pierwsza'}" @click="selectDistrict('pierwsza');">
+                I LIGA
+            </button>
+            <button :class="{'active': selectedDistrict === 'druga'}" to="#druga" @click="selectDistrict('druga');">
+                II LIGA
+            </button>
+            </div>
+    </Section>
+   
+    <Section anchor="Teams" grid="4" >
+      <div v-if="selectedDistrict === 'pierwsza'" v-for="item in pierwsza" :key="item.title" class="teams__card" @click="toggleCard(item)">
+        <div class="teams__card--front" :class="{ 'hidden': item.flipped }">        
+          <div class="teams__card--icon">
+            <NuxtImg :alt="`Logo drużyny ${item.team}`" :src="`/images/teams/${item.shortcut}.png`"/>
+          </div>
+          <div class="teams__card--name">
+            <p>{{ item.team }}</p>
+          </div>
         </div>
-        <div class="teams__card--name">
-          <p>{{ item.team }}</p>
+
+        <div class="teams__card--back" :class="{ 'hidden': !item.flipped }">
+          <div class="teams__card--top">
+            <NuxtImg :alt="`Logo drużyny ${item.team}`" :src="`/images/teams/${item.shortcut}.png`"/>
+            <p>{{ item.team }}</p>
+          </div>
+          <p class="balance">W:{{ item.wins }} L:{{ item.loses}}</p>
+          <div class="teams__card--bot">
+            <div class="teams__card--lane">
+              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/top.png`"></NuxtImg>
+              <p>{{ item.top }}</p>
+            </div>
+            <div class="teams__card--lane">
+              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/jungle.png`"></NuxtImg>
+              <p>{{ item.jg }}</p>
+            </div>
+            <div class="teams__card--lane">
+              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/mid.png`"></NuxtImg>
+              <p>{{ item.mid }}</p>
+            </div>
+            <div class="teams__card--lane">
+              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/adc.png`"></NuxtImg>
+              <p>{{ item.adc }}</p>
+            </div>
+            <div class="teams__card--lane">
+              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/support.png`"></NuxtImg>
+              <p>{{ item.sup }}</p>
+            </div>
+          </div>
         </div>
+        
+      </div>
+      <div v-if="selectedDistrict === 'druga'" v-for="item in druga" :key="item.title" class="teams__card" @click="toggleCard(item)">
+        <div class="teams__card--front" :class="{ 'hidden': item.flipped }">        
+          <div class="teams__card--icon">
+            <NuxtImg :alt="`Logo drużyny ${item.team}`" :src="`/images/teams/${item.shortcut}.png`"/>
+          </div>
+          <div class="teams__card--name">
+            <p>{{ item.team }}</p>
+          </div>
+        </div>
+
+        <div class="teams__card--back" :class="{ 'hidden': !item.flipped }">
+          <div class="teams__card--top">
+            <NuxtImg :alt="`Logo drużyny ${item.team}`" :src="`/images/teams/${item.shortcut}.png`"/>
+            <p>{{ item.team }}</p>
+          </div>
+          <p class="balance">W:{{ item.wins }} L:{{ item.loses}}</p>
+          <div class="teams__card--bot">
+            <div class="teams__card--lane">
+              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/top.png`"></NuxtImg>
+              <p>{{ item.top }}</p>
+            </div>
+            <div class="teams__card--lane">
+              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/jungle.png`"></NuxtImg>
+              <p>{{ item.jg }}</p>
+            </div>
+            <div class="teams__card--lane">
+              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/mid.png`"></NuxtImg>
+              <p>{{ item.mid }}</p>
+            </div>
+            <div class="teams__card--lane">
+              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/adc.png`"></NuxtImg>
+              <p>{{ item.adc }}</p>
+            </div>
+            <div class="teams__card--lane">
+              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/support.png`"></NuxtImg>
+              <p>{{ item.sup }}</p>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </Section>
     <Section anchor="Contact" title="Contact" subtitle="Build amazing web sites." grid="1">
@@ -51,6 +139,8 @@
     name: 'Home',
     data() {
       return {
+        selectedDistrict: 'pierwsza',
+      previousDistrict: null,
         about: {
           title: 'O nas',
           description: 'blabla',
@@ -166,25 +256,24 @@
           sup: 'dawid',
           opgg: 'https://www.op.gg/summoners/euw/Adison-2137'
         },
-      ].sort((a, b) => b.wins - a.wins),
-      druga: [
+      ],
+      druga: [      
         {
-          team: 'BEIFENG ESPORTS',
-          shortcut: 'BE',
-          wins: 21,
-          loses: 13,
+          team: 'BLOODY WOUNDS',
+          shortcut: 'BW',
+          wins: 5,
+          loses: 3,
           top: 'Grundi',
           jg: 'Lissek',
           mid: 'px3ke',
           adc: 'ek1pa',
           sup: 'dawid',
           opgg: ''
-        },
-        {
-          team: 'BLOODY WOUNDS',
-          shortcut: 'BW',
-          wins: 5,
-          loses: 3,
+        },{
+          team: 'BEIFENG ESPORTS',
+          shortcut: 'BE',
+          wins: 21,
+          loses: 13,
           top: 'Grundi',
           jg: 'Lissek',
           mid: 'px3ke',
@@ -270,7 +359,7 @@
           captainDC: 'px3ke#1234',
           shortcut: 'TS',
           wins: 13,
-          loses: 21,
+          loses: 10,
           top: 'Grundi',
           jg: 'Lissek',
           mid: 'px3ke',
@@ -278,7 +367,8 @@
           sup: 'dawid',
           opgg: ''
         },
-      ].sort((a, b) => b.wins - a.wins),
+      ],
+      
         contact: {
           title: 'Get in touch with us to create lightning-fast websites.',
           description: 'Our team specializes in creating lightning-fast websites that deliver exceptional user experiences. We are dedicated to building web solutions optimized for speed and reliability. Contact us today to discuss how we can help you achieve your website performance goals.',
@@ -288,6 +378,16 @@
           btnCall: 'Call us',
         }
       }
-    }
+    },
+    methods: {
+    toggleContent(item) {
+      // Toggle the selected item between null and the clicked item
+      this.selectedItem = this.selectedItem === item ? null : item;
+    },
+    selectDistrict(district) {
+    this.selectedDistrict = district;
+  },
+  },
+    
   }
 </script>
