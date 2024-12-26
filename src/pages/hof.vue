c3f9f9e80199e2a2c6208134d1ff332a687a7f19
JustJabbaaaa <adriankotowski217@gmail.com>
2024-12-26 17:56:06 +0100

    updated leagues

--- src/pages/hof.vue
+++ src/pages/hof.vue
@@ -8,7 +8,23 @@ const pageTitle = 'Ściana chwały';
 <template>
     <Navbar />
         <Section title="Ściana chwały">
-            
+            <UCollapsible class="flex flex-col gap-2 w-48">
+    <UButton
+      class="group"
+      label="Open"
+      color="neutral"
+      variant="subtle"
+      trailing-icon="i-lucide-chevron-down"
+      :ui="{
+        trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200'
+      }"
+      block
+    />
+
+    <template #content>
+      <Placeholder class="h-48" />xd
+    </template>
+  </UCollapsible>
         </Section>
     <Footer />
 </template>
--- src/pages/liga2.vue
+++ src/pages/liga2.vue
@@ -27,25 +27,9 @@ const pageTitle = 'Druga Liga';
           </div>
           <p class="balance">W:{{ item.wins }} L:{{ item.loses}}</p>
           <div class="teams__card--bot">
-            <div class="teams__card--lane">
-              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/top.png`"></NuxtImg>
-              <p>{{ item.top }}</p>
-            </div>
-            <div class="teams__card--lane">
-              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/jungle.png`"></NuxtImg>
-              <p>{{ item.jg }}</p>
-            </div>
-            <div class="teams__card--lane">
-              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/mid.png`"></NuxtImg>
-              <p>{{ item.mid }}</p>
-            </div>
-            <div class="teams__card--lane">
-              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/adc.png`"></NuxtImg>
-              <p>{{ item.adc }}</p>
-            </div>
-            <div class="teams__card--lane">
-              <NuxtImg :alt="`Ikona Toplane`" :src="`/images/linie/support.png`"></NuxtImg>
-              <p>{{ item.sup }}</p>
+            <div v-for="(value, key) in item.linie" class="teams__card--lane">
+              <NuxtImg :alt="`Ikona ${key}`" :src="`/images/linie/${key}.png`"></NuxtImg>
+              <p>{{value}}</p>
             </div>
           </div>
         </div>
@@ -64,95 +48,104 @@ export default {
           shortcut: 'BW',
           wins: 5,
           loses: 3,
-          top: 'Grundi',
-          jg: 'Lissek',
-          mid: 'px3ke',
-          adc: 'ek1pa',
-          sup: 'dawid',
-          opgg: ''
-        },{
+          linie: {
+            top: 'Grundi',
+            jg: 'Lissek',
+            mid: 'px3ke',
+            adc: 'ek1pa',
+            sup: 'dawid',
+          }
+        },
+        {
           team: 'BEIFENG ESPORTS',
           shortcut: 'BE',
           wins: 21,
           loses: 13,
-          top: 'Grundi',
-          jg: 'Lissek',
-          mid: 'px3ke',
-          adc: 'ek1pa',
-          sup: 'dawid',
-          opgg: ''
+          linie: {
+            top: 'aha',
+            jg: 'Lissek',
+            mid: 'px3ke',
+            adc: 'ek1pa',
+            sup: 'dawid',
+          }
         },
         {
           team: 'Chaos Banes',
           shortcut: 'CB',
           wins: 5,
           loses: 3,
-          top: 'Grundi',
-          jg: 'Lissek',
-          mid: 'px3ke',
-          adc: 'ek1pa',
-          sup: 'dawid',
-          opgg: ''
+          linie: {
+            top: 'Grundi',
+            jg: 'Lissek',
+            mid: 'px3ke',
+            adc: 'ek1pa',
+            sup: 'dawid',
+          }
         },
         {
           team: 'Chaos Harbringers',
           shortcut: 'CE',
           wins: 5,
           loses: 3,
-          top: 'Grundi',
-          jg: 'Lissek',
-          mid: 'px3ke',
-          adc: 'ek1pa',
-          sup: 'dawid',
-          opgg: ''
+          linie: {
+            top: 'Grundi',
+            jg: 'Lissek',
+            mid: 'px3ke',
+            adc: 'ek1pa',
+            sup: 'dawid',
+          }
         },
         {
           team: 'Gejmingowi Emeryci',
           shortcut: 'GEJ',
           wins: 5,
           loses: 3,
-          top: 'Grundi',
-          jg: 'Lissek',
-          mid: 'px3ke',
-          adc: 'ek1pa',
-          sup: 'dawid',
-          opgg: ''
+          linie: {
+            top: 'Grundi',
+            jg: 'Lissek',
+            mid: 'px3ke',
+            adc: 'ek1pa',
+            sup: 'dawid',
+          }
         },
         {
           team: 'No.7',
           shortcut: 'N7',
           wins: 5,
           loses: 3,
-          top: 'Grundi',
-          jg: 'Lissek',
-          mid: 'px3ke',
-          adc: 'ek1pa',
-          sup: 'dawid',
-          opgg: ''
+          linie: {
+            top: 'Grundi',
+            jg: 'Lissek',
+            mid: 'px3ke',
+            adc: 'ek1pa',
+            sup: 'dawid',
+          }
         },
         {
           team: 'Chaos Oni',
           shortcut: 'ONI',
           wins: 6,
           loses: 3,
-          top: 'Grundi',
-          jg: 'Lissek',
-          mid: 'px3ke',
-          adc: 'ek1pa',
-          sup: 'dawid',
-          opgg: ''
+          linie: {
+            top: 'Grundi',
+            jg: 'Lissek',
+            mid: 'px3ke',
+            adc: 'ek1pa',
+            sup: 'dawid',
+          }
         },
         {
           team: 'Reve Esports',
           shortcut: 'REV',
           wins: 8,
           loses: 3,
-          top: 'Grundi',
-          jg: 'Lissek',
-          mid: 'px3ke',
-          adc: 'ek1pa',
-          sup: 'dawid',
-          opgg: ''
+          linie: {
+            top: 'Grundi',
+            jg: 'Lissek',
+            mid: 'px3ke',
+            adc: 'ek1pa',
+            sup: 'dawid',
+          }
         },
         {
           team: 'Tuesday Slots',
@@ -161,12 +154,13 @@ export default {
           shortcut: 'TS',
           wins: 13,
           loses: 10,
-          top: 'Grundi',
-          jg: 'Lissek',
-          mid: 'px3ke',
-          adc: 'ek1pa',
-          sup: 'dawid',
-          opgg: ''
+          linie: {
+            top: 'Grundi',
+            jg: 'Lissek',
+            mid: 'px3ke',
+            adc: 'ek1pa',
+            sup: 'dawid',
+          }
         },
       ],
     }
